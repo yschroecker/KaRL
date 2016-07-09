b2c2466d62db28ed7d8c6c4c48413e3b7267ca0e
Yannick Schroecker <yannickschroecker@gmail.com>
2016-07-08 22:49:23 -0400

    more cartpole parmeterization

--- examples/cartpole/cartpole.py
+++ examples/cartpole/cartpole.py
@@ -16,24 +16,20 @@ class QNetwork:
         hidden1_size = 16
         hidden2_size = 16
         output_size = len(self.discretized_actions)
-        regularization = 0.00
 
         hidden1_W = tf.get_variable("hidden1_W", shape=[input_size, hidden1_size],
-                                    regularizer=tf.contrib.layers.l2_regularizer(regularization),
                                     initializer=tf.truncated_normal_initializer(mean=0, stddev=1/input_size))
         hidden1_b = tf.get_variable("hidden1_b", shape=[hidden1_size], initializer=tf.constant_initializer(0.))
         hidden1 = tf.nn.relu(tf.nn.bias_add(tf.matmul(state, hidden1_W), hidden1_b), name="hidden1")
 
         hidden2_W = tf.get_variable("hidden2_W", shape=[hidden1_size, hidden2_size],
-                                    regularizer=tf.contrib.layers.l2_regularizer(regularization),
                                     initializer=tf.truncated_normal_initializer(mean=0, stddev=1/hidden1_size))
         hidden2_b = tf.get_variable("hidden2_b", shape=[hidden2_size], initializer=tf.constant_initializer(0.))
         hidden2 = tf.nn.relu(tf.nn.bias_add(tf.matmul(hidden1, hidden2_W), hidden2_b), name="hidden2")
 
         output_W = tf.get_variable("output_W", shape=[hidden2_size, output_size],
-                                   regularizer=tf.contrib.layers.l2_regularizer(regularization),
                                    initializer=tf.truncated_normal_initializer(mean=0, stddev=1/hidden2_size))
-        output_b = tf.get_variable("output_b", shape=[output_size], initializer=tf.constant_initializer(0.))
+        output_b = tf.get_variable("output_b", shape=[output_size], initializer=tf.constant_initializer(1))
         return tf.nn.bias_add(tf.matmul(hidden2, output_W), output_b, name="Q")
 
 
@@ -51,9 +47,10 @@ if __name__ == '__main__':
         learner = dqn.DQN(network,
                           optimizer=tf.train.AdagradOptimizer(1e-1),
                           update_interval=1,
-                          freeze_interval=1,
+                          freeze_interval=100,
                           discount_factor=0.9,
                           experience_replay_memory=dqn.UniformExperienceReplayMemory(network.state_dim, 10000, 30),
+                          double_dqn=True,
                           global_step=global_step)
         exploration = dqn.EpsilonGreedy(learner, 0.0)
         session.run(tf.initialize_all_variables())
@@ -66,18 +63,17 @@ if __name__ == '__main__':
                 action = exploration.get_action(state)
                 next_state, reward, is_terminal, _ = env.step(action)
                 if is_terminal:
-                    reward = -100
-
-                learner.update(state, action, next_state, reward)
-                exploration.update()
-
-                cumulative_reward += reward
-                state = next_state
-                if is_terminal:
+                    learner.update(state, action, next_state, -100)
                     break
+                else:
+                    learner.update(state, action, next_state, reward)
+                    exploration.update()
+
+                    cumulative_reward += reward
+                    state = next_state
             last_100.append(cumulative_reward)
             last_100_mean = np.mean(last_100)
             print("Episode %d: %f(%f)" % (episode, last_100_mean, exploration._epsilon))
-            if last_100_mean > 95:
+            if last_100_mean > 195:
                 break
 
