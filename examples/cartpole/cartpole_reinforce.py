5bdc68280296a56d4954274ff3676d350901763d
Yannick Schroecker <yannickschroecker@googlemail.com>
2016-09-12 15:09:23 -0400

    main loop helper

--- examples/cartpole/cartpole_reinforce.py
+++ examples/cartpole/cartpole_reinforce.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
-import numpy as np
 import gym
 import sys
-import collections
 import algorithms.policy_gradient as pg
+import util.gym_env
+
 
 state_dim = [4]
 num_actions = 2
@@ -24,10 +24,7 @@ if __name__ == '__main__':
                 print("cartpole_dqn.py takes one argument: the output directory of openai gym monitor data")
                 sys.exit(1)
 
-            summary_dir = sys.argv[1] + '/summaries'
-
             env = gym.make("CartPole-v0")
-            #env.monitor.start(sys.argv[1] + '/monitor', force=True)
 
             policy = pg.DiscretePolicy(state_dim, num_actions, build_policy_network, optimizer)
             learner = pg.REINFORCE(policy=policy, state_dim=state_dim, discount_factor=discount_factor,
@@ -35,22 +32,6 @@ if __name__ == '__main__':
 
             session.run(tf.initialize_all_variables())
 
-            summary_writer = tf.train.SummaryWriter(summary_dir, session.graph)
+            util.gym_env.main_loop(env, learner, 200, num_iterations=1000, summary_frequency=None)
 
-            last_100 = collections.deque(maxlen=100)
-            for episode in range(10000):
-                state = env.reset()
-                cumulative_reward = 0
-                for t in range(200):
-                    action = learner.get_action(state)
-                    next_state, reward, is_terminal, _ = env.step(action)
-                    statistics = learner.update(state, action, next_state, reward, is_terminal)
-                    cumulative_reward += reward
-                    if is_terminal:
-                        break
-                    state = next_state
-                last_100.append(cumulative_reward)
-                last_100_mean = np.mean(last_100)
-                print("Episode %d: %f" % (episode, last_100_mean))
-                if len(last_100) == 100 and last_100_mean > 195:
-                    break
+
---
+++ util/gym_env.py
@@ -0,0 +1,40 @@
+import sys
+import tensorflow as tf
+import collections
+import numpy as np
+import tqdm
+
+
+def main_loop(env, learner, num_timesteps, num_iterations=10000, enable_monitor=False, summary_frequency=100,
+              do_render=False):
+    if len(sys.argv) < 2:
+        print("%s takes one argument: the output directory of monitor and summaries data" % sys.argv[0])
+        sys.exit(1)
+
+    summary_dir = sys.argv[1] + '/summaries'
+    summary_writer = tf.train.SummaryWriter(summary_dir, tf.get_default_session().graph)
+    if enable_monitor:
+        env.monitor.start(sys.argv[1] + '/monitor', force=True)
+
+    last_100 = collections.deque(maxlen=100)
+    trange = tqdm.trange(num_iterations)
+    for episode in trange:
+        state = env.reset()
+        cumulative_reward = 0
+        for t in range(num_timesteps):
+            if do_render:
+                env.render()
+            action = learner.get_action(state)
+            next_state, reward, is_terminal, _ = env.step(action)
+            learner.update(state, action, next_state, reward, is_terminal)
+            cumulative_reward += reward
+            if is_terminal:
+                break
+            state = next_state
+        last_100.append(cumulative_reward)
+        last_100_mean = np.mean(last_100)
+        trange.set_description("Episode %d: %f" % (episode, last_100_mean))
+        if len(last_100) == 100 and last_100_mean > 195:
+            break
+        if summary_frequency is not None and episode % summary_frequency == summary_frequency - 1:
+            learner.add_summaries(summary_writer, episode)
