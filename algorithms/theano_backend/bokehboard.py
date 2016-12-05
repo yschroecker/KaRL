52925f321ede85a9601fcff3868e77d7d2908595
Yannick Schroecker <yannickschroecker@googlemail.com>
2016-12-05 17:06:11 -0500

    faster bokehboard

--- algorithms/theano_backend/bokehboard.py
+++ algorithms/theano_backend/bokehboard.py
@@ -2,15 +2,18 @@ import bokeh
 import bokeh.models
 import bokeh.plotting
 import bokeh.client
+import bokeh.io
 import bokeh.layouts
+import time
 import numpy as np
+import collections
 
 
 plot_width = 800
 plot_height = 400
 
 
-class Bokehboard:
+class _Bokehboard:
     class _SimplePlot:
         def __init__(self, title):
             self._data = bokeh.models.ColumnDataSource(data={'x': [], 'y': []})
@@ -19,7 +22,7 @@ class Bokehboard:
             self._line.data_source = self._data
 
         def add_point(self, x, y):
-            self._data.data = {'x': self._data.data['x'] + [x], 'y': self._data.data['y'] + [y]}
+            self._data.stream({'x': [x], 'y': [y]})
 
     class _HistogramPlot:
         _quantiles = [66, 80, 95]
@@ -33,32 +36,37 @@ class Bokehboard:
             self._median_line = self.plot.line(x=[], y=[], line_width=5)
             self._median_line.data_source = self._median_data
 
-            self._quantile_data = {quantile: bokeh.models.ColumnDataSource(data={'x': [], 'y': []})
+            self._quantile_data = {quantile: bokeh.models.ColumnDataSource(data={'xs': [], 'ys': []})
                                    for quantile in self._quantiles}
-            self._patches = {quantile: self.plot.patch(x=[], y=[], fill_alpha=0.4 - 0.2*(i/len(self._quantiles)))
+            self._patches = {quantile: self.plot.patches(xs=[], ys=[], line_alpha=0,
+                                                         fill_alpha=0.4 - 0.2*(i/len(self._quantiles)))
                              for i, quantile in enumerate(self._quantiles)}
+            self._last_quantile_y = {quantile: [0, 0] for quantile in self._quantiles}
+            self._last_x = {quantile: 0 for quantile in self._quantiles}
             for quantile in self._quantiles:
                 self._patches[quantile].data_source = self._quantile_data[quantile]
 
         def add_point(self, x, ys):
-            self._mean_data.data.update(x=self._mean_data.data['x'] + [x],
-                                        y=self._mean_data.data['y'] + [np.mean(ys)])
-            self._median_data.data.update(x=self._median_data.data['x'] + [x],
-                                          y=self._median_data.data['y'] + [np.median(ys)])
+            self._mean_data.stream({'x': [x], 'y': [np.mean(ys)]})
+            self._median_data.stream({'x': [x], 'y': [np.median(ys)]})
             for quantile in self._quantiles:
-                self._quantile_data[quantile].data.update(x=[x] + self._quantile_data[quantile].data['x'] + [x],
-                                                          y=[np.percentile(ys, 100-quantile)] +
-                                                            self._quantile_data[quantile].data['y'] +
-                                                            [np.percentile(ys, quantile)])
+                last_y = self._last_quantile_y[quantile]
+                y = [np.percentile(ys, 100-quantile), np.percentile(ys, quantile)]
+                self._quantile_data[quantile].stream({'xs': [[self._last_x, x, x, self._last_x]],
+                                                      'ys': [[last_y[0], y[0], y[1], last_y[1]]]})
+                self._last_quantile_y[quantile] = y
+            self._last_x = x
 
     LINE_PLOT = 0
     HISTOGRAM_PLOT = 1
 
-    def __init__(self, session_name="default", plot_frequency=50):
+    def __init__(self, session_name=None, plot_frequency=1):
+        bokeh.io.curstate().autoadd = False
         self._watches = []
         self._python_vars = {}
         self._plot_frequency = plot_frequency
         self._update_counter = 0
+        self._last_update = time.time()
         self._session_name = session_name
 
         self._session = None
@@ -93,12 +101,13 @@ class Bokehboard:
             plots.append(plot.plot)
         layout = bokeh.layouts.gridplot(plots, ncols=2)
         doc.add_root(layout)
-        bokeh.client.show_session(self._session_name)
+        bokeh.client.show_session(self._session.id)
 
     def update(self):
         self._update_counter += 1
-
-        if self._update_counter % self._plot_frequency == 0:
+        current_time = time.time()
+        if self._last_update + self._plot_frequency <= current_time:
+            self._last_update = current_time
             self._update()
 
     def _update(self):
@@ -112,12 +121,12 @@ class Bokehboard:
         self._session.loop_until_closed()
 
 
-class BokehboardSummaryCreator:
+class Bokehboard:
     instance = None
 
     def __init__(self):
-        if not self.instance:
-            self.instance = Bokehboard()
+        if not Bokehboard.instance:
+            Bokehboard.instance = _Bokehboard()
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
@@ -126,12 +135,12 @@ class BokehboardSummaryCreator:
 if __name__ == '__main__':
     import theano
     import time
-    bb = Bokehboard(plot_frequency=1, session_name="wha")
+    bb = Bokehboard()
     var = theano.shared([1, 1, 1], name="test")
-    bb.add_tensor_variable("test", var, Bokehboard.HISTOGRAM_PLOT)
-    bb.add_tensor_variable("test2", var, Bokehboard.HISTOGRAM_PLOT)
-    bb.add_tensor_variable("test3", var, Bokehboard.HISTOGRAM_PLOT)
-    bb.add_python_variable("test4", Bokehboard.LINE_PLOT, test4=3)
+    bb.add_tensor_variable("test", var, bb.HISTOGRAM_PLOT)
+    bb.add_tensor_variable("test2", var, bb.HISTOGRAM_PLOT)
+    bb.add_tensor_variable("test3", var, bb.HISTOGRAM_PLOT)
+    bb.add_python_variable("test4", bb.LINE_PLOT, test4=3)
     bb.show()
     for i in range(1000):
         time.sleep(1)
--- util/gym_env.py
+++ util/gym_env.py
@@ -6,21 +6,63 @@ import glob
 import os
 import re
 import shutil
+import scipy.stats as stats
+import algorithms.theano_backend.bokehboard
+
+
+def load_last(learner, model_directory, save_model_frequency, save_model_horizon):
+    backups = glob.glob('%s/__backup_episode_*' % model_directory)
+    episodes = [int(re.findall(r'\d+', backup)[-1]) for backup in backups]
+    start_iteration = np.max(episodes)
+    learner.load(model_directory, "backup_%d" % ((start_iteration//save_model_frequency) % save_model_horizon))
+    return learner
+
+
+def collect_trajectories(env, learner, num_time_steps, num_iterations, reward_horizon=100, silent=False,
+                         discount_factor=1):
+    states = []
+    actions = []
+    rewards = []
+    trange = tqdm.trange(num_iterations, disable=silent)
+    for episode in trange:
+        state = env.reset()
+        cumulative_reward = 0
+        t = 0
+        episode_states = [state]
+        episode_actions = []
+        while num_time_steps is None or t < num_time_steps:
+            action = learner.get_action(state)
+            state, reward, is_terminal, _ = env.step(action)
+            episode_states.append(state)
+            episode_actions.append(action)
+            cumulative_reward += reward * discount_factor**t
+            t += 1
+            if is_terminal:
+                break
+        states.append(episode_states)
+        actions.append(episode_actions)
+        rewards.append(cumulative_reward)
+        trange.set_description("Episode %d: %f +/- %f" % (episode, np.mean(rewards[-reward_horizon:]),
+                                                          stats.sem(rewards[-reward_horizon:])))
+    return states, actions, rewards
 
 
 def main_loop(env, learner, num_time_steps, reward_horizon=100, reward_threshold=None, num_iterations=10000,
-              enable_monitor=False,
-              summary_frequency=100, do_render=False, save_model_directory=None, save_model_frequency=10,
-              save_model_horizon=10, restore=False):
+              enable_monitor=False, discount_factor=1,
+              summary_frequency=None, do_render=False, save_model_directory=None, save_model_frequency=10,
+              save_model_horizon=10, restore=False, episodic=True, episode_hooks=[], update_every=1):
     sys.setrecursionlimit(50000)
+    bokehboard = algorithms.theano_backend.bokehboard.Bokehboard()
+    bokehboard.show()
 
-    if len(sys.argv) < 2:
-        print("%s takes one argument: the output directory of monitor and summaries data" % sys.argv[0])
-        sys.exit(1)
+    if summary_frequency is not None:
+        if len(sys.argv) < 2:
+            print("%s takes one argument: the output directory of monitor and summaries data" % sys.argv[0])
+            sys.exit(1)
 
-    summary_dir = sys.argv[1] + '/summaries'
-    if enable_monitor:
-        env.monitor.start(sys.argv[1] + '/monitor', force=True)
+        summary_dir = sys.argv[1] + '/summaries'
+        if enable_monitor:
+            env.monitor.start(sys.argv[1] + '/monitor', force=True)
 
     reward_window = collections.deque(maxlen=reward_horizon)
     if restore:
@@ -39,18 +81,25 @@ def main_loop(env, learner, num_time_steps, reward_horizon=100, reward_threshold
             os.makedirs(save_model_directory)
 
     trange = tqdm.trange(start_iteration, num_iterations)
-    for episode in trange:
+    if not episodic:
         state = env.reset()
+    for episode in trange:
+        if episodic:
+            state = env.reset()
+        for hook in episode_hooks:
+            hook(learner, episode)
         cumulative_reward = 0
         t = 0
         while num_time_steps is None or t < num_time_steps:
+            bokehboard.update()
             t += 1
             if do_render:
                 env.render()
             action = learner.get_action(state)
             next_state, reward, is_terminal, _ = env.step(action)
-            update_result = learner.update(state, action, next_state, reward, is_terminal)
-            cumulative_reward += reward
+            if (episode * num_time_steps + t) % update_every == 0:
+                update_result = learner.update(state, action, next_state, reward, is_terminal)
+            cumulative_reward += reward * discount_factor**t
             if is_terminal:
                 break
             state = next_state
